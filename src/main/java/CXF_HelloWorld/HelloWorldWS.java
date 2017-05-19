e7c0d54c953e97749a9a53a46c73be9cfe91596f
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2017-05-19 09:18:09 -0700

    Incomplete

---
+++ src/main/java/CXF_HelloWorld/HelloWorldWS.java
@@ -0,0 +1,26 @@
+package CXF_HelloWorld;
+
+/**
+ * Created by ricardo on 5/19/17.
+ */
+@WebService
+public interface HelloWorld {
+
+    String sayHi(@WebParam(name="text") String text);
+
+
+    /* Advanced usecase of passing an Interface in.  JAX-WS/JAXB does not
+     * support interfaces directly.  Special XmlAdapter classes need to
+     * be written to handle them
+     */
+    String sayHiToUser(User user);
+
+
+    /* Map passing
+     * JAXB also does not support Maps.  It handles Lists great, but Maps are
+     * not supported directly.  They also require use of a XmlAdapter to map
+     * the maps into beans that JAXB can use.
+     */
+    @XmlJavaTypeAdapter(IntegerUserMapAdapter.class)
+    Map<Integer, User> getUsers();
+}
